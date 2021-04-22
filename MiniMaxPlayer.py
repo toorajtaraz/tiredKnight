ef827e994eb46e25540f3520fa4a4a6b5581f2fa
tooraj taraz <tooraj.info@gmail.com>
2021-04-22 17:21:37 +0430

    AI chess player using minimax algorithm

---
+++ MiniMaxPlayer.py
@@ -0,0 +1,61 @@
+import chess
+from Player import Player
+from ai import *
+
+class MiniMaxPlayer(Player):
+    max_depth = 2
+    move_count = 0
+    def evaluate(self, board):
+        total = 0
+        end_game = are_we_in_end_game(board)
+
+        for square in chess.SQUARES:
+            piece = board.piece_at(square)
+            if not piece:
+                continue
+
+            value = piece_value[piece.piece_type] + evaluate_position(piece, square, end_game)
+            if piece.color == chess.WHITE:
+                total += value
+            else:
+                total -= value
+
+        return total
+
+
+    def move(self, board):
+        value = -float('inf') if self.player_color else float('inf')
+        best_move = None
+        legal_moves = list(board.legal_moves)
+        for move in legal_moves:
+            board.push(move)
+            if board.is_checkmate():
+                board.pop()
+                return move
+            if board.is_game_over():
+                value = max(value, self.evaluate(board)) if self.player_color else min(value, self.evaluate(board))
+                if self.player_color:
+                    if temp >= value:
+                        value = temp
+                        best_move = move
+                else:
+                    if temp <= value:
+                        value = temp
+                        best_move = move
+
+                board.pop()
+                continue
+            temp = minimizer(board, self.max_depth, self.evaluate) if self.player_color else maximizer(board, self.max_depth, self.evaluate)
+            if self.player_color:
+                if temp >= value:
+                    value = temp
+                    best_move = move
+            else:
+                if temp <= value:
+                    value = temp
+                    best_move = move
+            board.pop()
+        print(value)
+        self.move_count += 1
+        return best_move
+
